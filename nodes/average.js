53c40cb94e4e97f197a2b01283a02747fd2e03e7
Marian Rudzynski <marian@rudzyn.ski>
2021-10-23 10:02:47 -0700

    initial, add barebones average node

---
+++ nodes/average.js
@@ -0,0 +1,26 @@
+const lodash = require("lodash");
+const { sum } = lodash;
+
+module.exports = function (RED) {
+  function AverageNode(config) {
+    RED.nodes.createNode(this, config);
+    const node = this;
+    const context = node.context();
+
+    node.on("input", async (msg) => {
+      const { topic, payload } = msg;
+      if (!topic || topic == "") return;
+
+      const values = {
+        [topic]: payload,
+        ...context.get("values"),
+      };
+      context.set("values", values);
+
+      const average = sum(Object.values(values)) / Object.keys(values).length;
+      node.send({ payload: average });
+    });
+  }
+
+  RED.nodes.registerType("average", AverageNode);
+};
